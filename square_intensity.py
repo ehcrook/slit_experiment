7d88732e1a7e4dca83e4156da4194242b26f846f
rhina99 <42660669+rhina99@users.noreply.github.com>
2018-11-04 04:56:28 -0500

    Add files via upload

---
+++ square_intensity.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def square_intensity(a, N, D, l):
+    x = np.linspace(-10, 10, 10000)
+    y = np.linspace(-20, 20, 10000)
+    y = list()
+    for val in x:
+        stuff_x = ((2*np.pi*a*val) / (l*D))
+        stuff_y = ((2*np.pi*a*val) / (l*D))
+        j = ((math.sin(stuff_x)) / (stuff_x))**2
+        k = ((math.sin(stuff_y)) / (stuff_y))**2
+        i = j * k
+        y.append(i)
+
+    plt.plot(x, y)
+    plt.show()
+    return[x, y]
+
+
+
+#test
+
+def input_():
+    #user inputs
+    l = input("enter a wavelength (nm): ")
+    l = float(l) * (10**-9)
+    a = input("enter a slit width (micro m): ")      #for single slit
+    a = float(a) * (10**-6)
+    d = input("enter a slit separation (micro m): ") #for double slit
+    d = float(d) * (10**-10)
+    D = input("enter a distance from the screen (m): ")
+    D = float(D)
+    N = input("enter number of slits: ")             #for n slit
+    N = int(N)
+    #num = input("finally, input the number of particles: ")
+    #num = int(num)
+
+    square_intensity(a, N, D, l)
+
+
+#run
+input_()
+
